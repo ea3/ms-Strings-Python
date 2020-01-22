2333de07f89d813913b4e885ee69a2e4ba364b72
Emilio Araos <ea3dev@gmail.com>
2020-01-21 21:37:47 -0700

    Sliceback file

--- strings2.py
+++ strings2.py
@@ -1,11 +1,16 @@
 parrot = "Norwegian Blue"
-print(parrot)
-print(parrot[3])
+# print(parrot[:6])   # Start at position zero, up to position number 5
+# print(parrot[0:9])
+# print(parrot[10:14])
+# print(parrot[:14])
 
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
-print(parrot[-1])
-print(parrot[-2])
+# Using step slicing:
+
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "9,223;372:036 854,775;807"
+separators = number[1::4]
+print(separators)
+values = "".join(char if char not in separators else " " for char in number). split()
+print([int(val) for val in values])
